e1c1291c69d060efedb6d5089a160021cf703462
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 <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>
2025-06-04 09:33:54 +0100

    test

--- src/app/api/candidats/register/route.ts
+++ src/app/api/candidats/register/route.ts
@@ -4,7 +4,6 @@ import CandidatPromise from '../../../../models/Candidats';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { ApiResponse } from '@/lib/types';
-import { ApiResponse } from '@/lib/types';
 
 
 interface RegisterResponse {
